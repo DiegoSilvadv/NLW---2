34d5250673287768a80fdd24d91f9a0159dcf211
DiegoSilvadv <digssilva2000@outlook.com>
2020-08-05 17:59:04 -0300

    Iniciando com javascript

---
+++ scripts/addField.js
@@ -0,0 +1,19 @@
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+
+function cloneField(){
+    // cloneNode() se for true pega todo o elemento filho e replica para o html
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    //adicionar a campos na pagina
+    const fields = newFieldContainer.querySelectorAll('input')
+    //para cada field vai executar algo
+    fields.forEach(function(field) {
+        fields.value = ""
+    })
+   
+
+
+
+    
+    document.querySelector('#schedules-items').appendChild(newFieldContainer)
+}
