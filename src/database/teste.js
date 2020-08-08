7d7f6949d3ab75f7f37a4964818c892c6b99716a
DiegoSilvadv <60052506+DiegoSilvadv@users.noreply.github.com>
2020-08-07 23:53:49 -0300

    Finalizando aplicação

--- src/database/teste.js
+++ src/database/teste.js
@@ -15,13 +15,11 @@ Database.then( async (db) => {
         whatsapp: "12665523125", 
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório epor mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     }
-
     classValue = {
         subject: 1, 
         cost: "20", 
     }
     //class_id virá pelo banco de dados, após cadastrarmos a class
-
     classScheduleValues = [
         {
             weekday: 1, 
@@ -35,9 +33,36 @@ Database.then( async (db) => {
             time_to: 1220
         }
     ] 
-    
-
-    await createProffy(db, {proffyValue, classValue, classScheduleValues })
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues })
 
     //consultar dados
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    //consultar as classes de um determinado professor
+    //e trazer junto os dados do professor 
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 14;
+    `)
+    //console.log(selectedClassesAndProffys)
+
+    // O horário que a pessoa trabalha, por exemplo é das 8h - 18h
+    //O horário do time_from (8) precisa ser menor ou igual ao horário solicitado
+    //O time_to precisa ser acima
+    const selectClassesSchedule = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 14
+        AND class_schedule.weekday = "2"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520";
+    `)
+
+    console.log(selectClassesSchedule)
+
+
 })
